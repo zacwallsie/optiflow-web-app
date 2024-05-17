82bf3c14de31452d524d1e17587606185d68cca4
Zachary Walls <zacwalls20@gmail.com>
2024-05-17 11:45:54 +1000

    Update to Docker Compose

--- backend/strartserver.sh
+++ backend/strartserver.sh
@@ -1,6 +1,2 @@
-if  [ "${DJANGO_SETTINGS_MODULE}" = "django_backend.dev_settings" ]
-then
-    python manage.py runserver 0.0.0.0:8000
-else
-    gunicorn --bind :8000 --workers 4 django_backend.wsgi:application
+gunicorn --bind :8000 --workers 4 backend.wsgi:application
 fi
