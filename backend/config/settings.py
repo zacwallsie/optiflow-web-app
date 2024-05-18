f6ab63c47badddd22ebd4c85251ab34268f9f99b
Zachary Walls <zacwalls20@gmail.com>
2024-05-18 13:56:25 +1000

    Updated Build

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -216,6 +216,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React App will be on this port
     "http://127.0.0.1:9000",
     "http://170.64.194.113:3000",
+    "http://127.0.0.1:3000",
 ]
 
 
